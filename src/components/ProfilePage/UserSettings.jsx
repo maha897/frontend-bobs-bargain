8d80fd1460566ed35412eaa7a717c49f5f4f474d
msg <mats.gravem@no.experis.com>
2024-03-19 15:48:01 +0100

    Fix user setting route

--- src/components/ProfilePage/UserSettings.jsx
+++ src/components/ProfilePage/UserSettings.jsx
@@ -1,86 +1,90 @@
-import { useContext, useState } from "react"
-import { Context } from "../../App"
+import { useContext, useState } from "react";
+import { Context } from "../../App";
 import { useNavigate } from "react-router-dom";
 import AccountHeader from "./AccountHeader";
 
 function UserSettings() {
-    const { userLoggedIn, setUserLoggedIn, users, setUsers } = useContext(Context)
-    const [inputData, setInputData] = useState(userLoggedIn)
-    const navigate = useNavigate()
+  const { user } = useContext(Context);
+  const [inputData, setInputData] = useState(user);
+  const navigate = useNavigate();
 
-    function handleChange(event) {
-        const { name, value } = event.target
-        setInputData({ ...inputData, [name]: value})
-    }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setInputData({ ...inputData, [name]: value });
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const updatedUsers = users.map((user) => user.id === userLoggedIn.id ? inputData : user)
+  function handleSubmit(event) {
+    event.preventDefault();
 
-        setUsers(updatedUsers)
-        setUserLoggedIn(inputData)
-        navigate("/profile")
-    }
+    // TODO: PUT REQUEST
 
-    return (
-      <div className="user-settings">
-        <AccountHeader />
+    navigate("/profile");
+  }
 
-        <form className="user-settings-form" onSubmit={handleSubmit}>
-          <label htmlFor="firstName">First name</label> <br />
-          <input
-            type="text"
-            name="firstName"
-            onChange={handleChange}
-            value={inputData.firstName}
-            required
-          />
-          <br />
-          <br />
-          <label htmlFor="lastName">Last name</label> <br />
-          <input
-            type="text"
-            name="lastName"
-            onChange={handleChange}
-            value={inputData.lastName}
-            required
-          />
-          <br />
-          <br />
-          <label htmlFor="email">Email</label> <br />
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            value={inputData.email}
-            required
-          />
-          <br />
-          <br />
-          <label htmlFor="phone">Phone</label> <br />
-          <input
-            type="number"
-            name="phone"
-            onChange={handleChange}
-            value={inputData.phone}
-            required
-          />
-          <br />
-          <br />
-          <label htmlFor="password">Password</label> <br />
-          <input
-            type="password"
-            name="password"
-            onChange={handleChange}
-            value={inputData.password}
-            required
-          />
-          <br />
-          <br />
-          <button type="submit">Save changes</button>
-        </form>
-      </div>
-    );
+  return (
+    <div className="user-settings">
+      <AccountHeader user={user} />
+
+      <form className="user-settings-form" onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First name</label>
+        <br />
+        <input
+          type="text"
+          name="firstName"
+          onChange={handleChange}
+          value={inputData.firstName}
+          required
+        />
+        <br />
+        <br />
+        <label htmlFor="lastName">Last name</label>
+        <br />
+        <input
+          type="text"
+          name="lastName"
+          onChange={handleChange}
+          value={inputData.lastName}
+          required
+        />
+        <br />
+        <br />
+        <label htmlFor="email">Email</label>
+        <br />
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={inputData.email}
+          required
+        />
+        <br />
+        <br />
+        <label htmlFor="phone">Phone</label>
+        <br />
+        <input
+          type="number"
+          name="phone"
+          onChange={handleChange}
+          value={inputData.phone}
+          required
+        />
+        <br />
+        <br />
+        <label htmlFor="password">Password</label>
+        <br />
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          value={inputData.password}
+          required
+        />
+        <br />
+        <br />
+        <button type="submit">Save changes</button>
+      </form>
+    </div>
+  );
 }
 
-export default UserSettings
+export default UserSettings;
