7b6912a18c02aeb3fee0cbc7fdda9781fba1b355
noahlenn <noahlenn@hotmail.com>
2024-03-20 15:41:12 +0100

    Proptype

--- src/components/BrowsePage/SearchBar.jsx
+++ src/components/BrowsePage/SearchBar.jsx
@@ -1,53 +1,58 @@
 import { useState, useEffect, useContext } from "react";
 import { fetchAllListings } from "../../service/api";
 import { Context } from "../../App";
+import PropTypes from "prop-types";
 
-function SearchBar({setFilteredAds}) {
-    const { token } = useContext(Context);
-    const [selectedCategory, setSelectedCategory] = useState("");
+function SearchBar({ setFilteredAds }) {
+  const { token } = useContext(Context);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
-    useEffect(() => {
-        if (selectedCategory !== "") {
-            fetchFilteredAds(selectedCategory);
-        } else {
-            // If no category is selected, fetch all listings
-            fetchAllListings(token).then((data) => setFilteredAds(data));
-        }
-    }, [selectedCategory]); // Fetch filtered ads whenever the selected category changes
+  useEffect(() => {
+    if (selectedCategory !== "") {
+      fetchFilteredAds(selectedCategory);
+    } else {
+      // If no category is selected, fetch all listings
+      fetchAllListings(token).then((data) => setFilteredAds(data));
+    }
+  }, [selectedCategory]); // Fetch filtered ads whenever the selected category changes
 
-    const fetchFilteredAds = async (category) => {
-        try {
-            const filteredListings = await fetchAllListings(token, null, category);
-            setFilteredAds(filteredListings);
-        } catch (error) {
-            console.error("Error fetching filtered listings:", error);
-        }
-    };
+  const fetchFilteredAds = async (category) => {
+    try {
+      const filteredListings = await fetchAllListings(token, null, category);
+      setFilteredAds(filteredListings);
+    } catch (error) {
+      console.error("Error fetching filtered listings:", error);
+    }
+  };
 
-    return (
-        <div className="search-bar-container">
-            <form className="search-bar">
-                <input name="search-query" placeholder="Search..."/>
-                <button type="submit">Search</button>
-            </form>
+  return (
+    <div className="search-bar-container">
+      <form className="search-bar">
+        <input name="search-query" placeholder="Search..." />
+        <button type="submit">Search</button>
+      </form>
 
-            <div className="category-dropdown">
-                <select
-                    value={selectedCategory}
-                    onChange={(event) => setSelectedCategory(event.target.value)}
-                    name="category"
-                >
-                    <option value="">All ads</option>
-                    <option value="electronics">Electronics and appliances</option>
-                    <option value="clothing">Clothing and cosmetics</option>
-                    <option value="furniture">Furniture and interior</option>
-                    <option value="vehicles">Vehicles</option>
-                    <option value="house">House and renovation</option>
-                    <option value="other">Other</option>
-                </select>
-            </div>
-        </div>
-    );
+      <div className="category-dropdown">
+        <select
+          value={selectedCategory}
+          onChange={(event) => setSelectedCategory(event.target.value)}
+          name="category"
+        >
+          <option value="">All ads</option>
+          <option value="electronics">Electronics and appliances</option>
+          <option value="clothing">Clothing and cosmetics</option>
+          <option value="furniture">Furniture and interior</option>
+          <option value="vehicles">Vehicles</option>
+          <option value="house">House and renovation</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+    </div>
+  );
 }
+
+SearchBar.propTypes = {
+  setFilteredAds: PropTypes.function,
+};
 
 export default SearchBar;
